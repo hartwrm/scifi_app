915ee245a6b702335261ca6a1faf5e9ffc5ddddf
hartwrm <47437949+hartwrm@users.noreply.github.com>
2019-08-26 21:23:00 -0400

    landing page and nest routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root to: "welcome#index"
+  resources :welcome, only: [:index]
   resources :images
-  resources :stories
-  resources :sentences
+  resources :sentences do
+    resources :stories
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
